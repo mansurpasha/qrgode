b29d9545939fe3742f531a7f1a1c9628377242c7
mansurpasha <mansurp@theodo.co.uk>
2022-08-04 15:53:40 +0100

    Create QrCode go board for entered text

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::io;
 use graphics::{ellipse, types::Color};
 use graphics_buffer::{IDENTITY, RenderBuffer};
+use qrcode::QrCode;
 
 fn main() {
     let mut buf = String::new();
@@ -11,38 +12,55 @@ fn main() {
         .read_line(&mut buf)
         .expect("Error reading line");
 
-    let _code = qrgode::create_qr_code(buf);
+    let code = qrgode::create_qr_code(buf);
 
-    create_image();
+    convert_qr_code_to_image(code,"test.png");
 }
 
-fn create_image() {
-    let mut buffer = RenderBuffer::new(100, 100);
-    // Brown background
+fn convert_qr_code_to_image(qr_code: QrCode, filename: &str) {
+    let code_width = qr_code.width();
+    let qr_code_matrix = qr_code.into_vec();
+    
+    let circle_radius: usize = 10; 
+    let image_width = circle_radius * 2 * code_width;
+    let mut buffer = RenderBuffer::new(image_width as u32, image_width as u32);
+
+    // Set background
     buffer.clear(BROWN);
 
-    let black_circle = Circle { centre: Point { x: 50.0, y: 50.0 }, radius: 50.0, color: BLACK };
-    draw_circle(&mut buffer, black_circle);
-    let white_circle = Circle { centre: Point { x: 25.0, y: 25.0 }, radius: 25.0, color: WHITE };
-    draw_circle(&mut buffer, white_circle);
+    // Draw circles
+    for x in 0..code_width {
+        for y in 0..code_width {
+            let item = code_width * y + x;
+            let diameter = circle_radius * 2;
+            let centre_x = diameter * x + circle_radius;
+            let centre_y = diameter * y + circle_radius;
+            println!("Code width is {}, x is {}, y is {}, item is {}", code_width, x, y, item);
+            let color = if qr_code_matrix[item] {
+                BLACK
+            } else {
+                WHITE
+            };
+            let circle = Circle {
+                centre: Point { x: centre_x as f64, y: centre_y as f64 },
+                radius: circle_radius as f64,
+                color,
+            };
+            draw_circle(&mut buffer, circle);
+        }
+    }
 
-    buffer.save("test.png").unwrap();
+    buffer.save(filename).unwrap();
 }
+
+const BLACK: Color = [0.0, 0.0, 0.0, 1.0];
+const WHITE: Color = [0.9, 0.9, 0.9, 1.0];
+const BROWN: Color = [139.0/255.0, 69.0/255.0, 19.0/255.0, 1.0];
 
 struct Point {
     x: f64,
     y: f64,
 }
-
-impl Clone for Point {
-    fn clone(&self) -> Self {
-        Point { x: self.x, y: self.y }
-    }
-}
-
-const BLACK: Color = [0.0, 0.0, 0.0, 1.0];
-const WHITE: Color = [1.0, 1.0, 1.0, 1.0];
-const BROWN: Color = [222.0/255.0, 184.0/255.0, 135.0/255.0, 1.0];
 
 struct Circle {
     centre: Point,
