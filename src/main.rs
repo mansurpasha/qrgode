ca911f8e237f92614a4189a577944c7015fd0cdd
mansurpasha <mansurp@theodo.co.uk>
2022-08-04 11:43:11 +0100

    Create blank image file

--- src/main.rs
+++ src/main.rs
@@ -10,4 +10,15 @@ fn main() {
         .expect("Error reading line");
 
     let _code = qrgode::create_qr_code(buf);
+
+    create_image();
 }
+
+fn create_image() {
+    let mut img = image::ImageBuffer::new(500, 500);
+    for pixel in img.pixels_mut() {
+        *pixel = image::Rgb::<u8>([20, 20, 20]);
+    }
+    img.save("test.png").unwrap();
+}
+
