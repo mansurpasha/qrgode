123ef12c9e0a6c19681838bca87d282099ac6f36
mansurpasha <mansurp@theodo.co.uk>
2022-08-04 14:36:26 +0100

    Use piston2d to draw a circle

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use std::io;
+use graphics::{ellipse, types::Color};
+use graphics_buffer::{IDENTITY, RenderBuffer};
 
 fn main() {
     let mut buf = String::new();
@@ -15,10 +17,33 @@ fn main() {
 }
 
 fn create_image() {
-    let mut img = image::ImageBuffer::new(500, 500);
-    for pixel in img.pixels_mut() {
-        *pixel = image::Rgb::<u8>([20, 20, 20]);
+    let mut buffer = RenderBuffer::new(100, 100);
+    // Brown background
+    buffer.clear([222.0, 184.0, 135.0, 1.0]);
+
+    let centre = Point::new(50.0, 50.0);
+    draw_circle(&mut buffer, centre, 50.0, [1.0, 0.0, 0.0, 1.0]);
+
+    buffer.save("test.png").unwrap();
+}
+
+struct Point {
+    x: f64,
+    y: f64,
+}
+
+impl Point {
+    fn new(x: f64, y:f64) -> Point {
+        Point { x, y }
     }
-    img.save("test.png").unwrap();
+}
+
+fn draw_circle(buffer: &mut RenderBuffer, centre: Point, radius: f64, color: Color) {
+    ellipse(
+        color,
+        [centre.x - radius, centre.y - radius, radius * 2.0, radius * 2.0],
+        IDENTITY,
+        buffer,
+    );
 }
 
