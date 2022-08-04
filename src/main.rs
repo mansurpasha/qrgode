1ba699f67fa522b776bbe514d7009e83c83127bb
mansurpasha <mansurp@theodo.co.uk>
2022-08-04 15:04:36 +0100

    Create reuseable circle drawing function

--- src/main.rs
+++ src/main.rs
@@ -19,10 +19,12 @@ fn main() {
 fn create_image() {
     let mut buffer = RenderBuffer::new(100, 100);
     // Brown background
-    buffer.clear([222.0, 184.0, 135.0, 1.0]);
+    buffer.clear(BROWN);
 
-    let centre = Point::new(50.0, 50.0);
-    draw_circle(&mut buffer, centre, 50.0, [1.0, 0.0, 0.0, 1.0]);
+    let black_circle = Circle { centre: Point { x: 50.0, y: 50.0 }, radius: 50.0, color: BLACK };
+    draw_circle(&mut buffer, black_circle);
+    let white_circle = Circle { centre: Point { x: 25.0, y: 25.0 }, radius: 25.0, color: WHITE };
+    draw_circle(&mut buffer, white_circle);
 
     buffer.save("test.png").unwrap();
 }
@@ -32,16 +34,27 @@ struct Point {
     y: f64,
 }
 
-impl Point {
-    fn new(x: f64, y:f64) -> Point {
-        Point { x, y }
+impl Clone for Point {
+    fn clone(&self) -> Self {
+        Point { x: self.x, y: self.y }
     }
 }
 
-fn draw_circle(buffer: &mut RenderBuffer, centre: Point, radius: f64, color: Color) {
+const BLACK: Color = [0.0, 0.0, 0.0, 1.0];
+const WHITE: Color = [1.0, 1.0, 1.0, 1.0];
+const BROWN: Color = [222.0/255.0, 184.0/255.0, 135.0/255.0, 1.0];
+
+struct Circle {
+    centre: Point,
+    radius: f64,
+    color: Color,
+}
+
+fn draw_circle(buffer: &mut RenderBuffer, circle: Circle) {
+    let Circle { centre, radius, color } = circle;
     ellipse(
         color,
-        [centre.x - radius, centre.y - radius, radius * 2.0, radius * 2.0],
+        [centre.x - circle.radius, centre.y - radius, radius * 2.0, radius * 2.0],
         IDENTITY,
         buffer,
     );
