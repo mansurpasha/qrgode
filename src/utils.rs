38a9dca99600b88af9440f6dea78ff7f8f567e57
mansurpasha <mansurp@theodo.co.uk>
2022-08-08 15:30:26 +0100

    Run linter

--- src/utils.rs
+++ src/utils.rs
@@ -1,6 +1,6 @@
 use qrcode::QrCode;
 
 pub fn create_qr_code(text: String) -> QrCode {
-    let code = QrCode::new(text.into_bytes()).unwrap();
-    return code;
+    
+    QrCode::new(text.into_bytes()).unwrap()
 }
